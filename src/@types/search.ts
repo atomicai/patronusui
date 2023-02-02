930d077fa2fd70375d10fd379382178a358a34fc
Dmitry Yurusov <dyurusov@gmail.com>
2023-02-02 21:38:38 +0300

    modify sample detailed text highlighting

--- src/@types/search.ts
+++ src/@types/search.ts
@@ -11,10 +11,20 @@ export type Passage = {
   docs: Doc[]
 }
 
+export enum DocPartColor {
+  highlighted = 'highlighted',
+  color1 = 'color1',
+  color2 = 'color2',
+  color3 = 'color3',
+  color4 = 'color4',
+  color5 = 'color4',
+}
+
 export type DocPart = {
   lo: number;
   hi: number;
   score?: string;
+  color?: DocPartColor;
 }
 
 export type Doc = {
@@ -24,7 +34,6 @@ export type Doc = {
   timestamp?: string
   upvote?: number
   highlight?: DocPart[];
-  colorify?: string[];
 }
 
 export type DataFromServer = {
--- src/components/SearchResult/DetailedDoc.tsx
+++ src/components/SearchResult/DetailedDoc.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode, useMemo } from 'react';
-import { Doc } from '../../@types/search';
+import { Doc, DocPartColor } from '../../@types/search';
 import { Tooltip } from './Tooltip';
 import styles from './DetailedDoc.module.css';
 
@@ -20,55 +20,33 @@ const highlightText = (doc: Doc) => {
   const highlightedText: ReactNode[] = [];
 
   // it is suggested that:
+  //  - there is no '\n' between lo and hi indexes
   //  - parts[i].lo <= parts[i].hi
   //  - parts[i].hi < parts[i + 1].lo
   let idx = 0;
   for (const part of (doc.highlight || [])) {
-    highlightedText.push(breakLines(doc.text.substring(idx, part.lo), doc));
+    highlightedText.push(breakLines(doc.text.substring(idx, part.lo)));
     highlightedText.push((
       <Tooltip key={idx} title={part.score}>
-          <div className={styles.highlighted}>
-            {breakLines(doc.text.substring(part.lo, part.hi + 1), doc)}
-          </div>
+          <span className={styles[part.color || DocPartColor.highlighted]}>
+            {doc.text.substring(part.lo, part.hi + 1)}
+          </span>
       </Tooltip>
     ))
     idx = part.hi + 1;
   }
-  highlightedText.push(breakLines(doc.text.substring(idx), doc));
+  highlightedText.push(breakLines(doc.text.substring(idx)));
 
   return highlightedText;
 };
 
-const breakLines = (text: string, doc: Doc) => {
+const breakLines = (text: string) => {
   const parts = text.split('\n');
   const result: ReactNode[] = [];
-  for (let idx = 0; idx < parts.length; idx++) {
-    result.push((
-      <div key={idx} className={styles.cue}>
-        {decorateActor(parts[idx], doc.colorify || [])}
-      </div>
-    ))
-  }
-  return result;
-}
-
-const decorateActor = (text: string, actors: string[], actorIdx = 0) => {
-  if (actorIdx >= actors.length) {
-    return text;
-  }
-
-  const actor = actors[actorIdx];
-  const parts = text.split(actor);
-  const result: ReactNode[] = [];
-  let el = parts.shift();
-  let key = 0;
-  while (el !== undefined) {
-    result.push(decorateActor(el, actors, actorIdx + 1));
-    result.push(<span key={key} className={styles[`actor${actorIdx % 3}`]}>{actor}</span>);
-    el = parts.shift();
-    key++;
+  for (const part of parts) {
+    result.push(part);
+    result.push(<div className={styles.newLine} />);
   }
   result.pop();
-
   return result;
 }
