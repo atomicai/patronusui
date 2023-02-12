32f00abfd3ca2e64c10b70886a0f11a6b146c36e
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-02-12 21:09:21 +0300

    Enhance profile page
    * Remove obsolete comments
    * Add custom classnames

--- src/pages/user/Profile.tsx
+++ src/pages/user/Profile.tsx
@@ -14,9 +14,6 @@ import styles from './components/styles/Profile.module.css'
 
 function Profile() {
   const [userAtom] = useAtom(currentUser)
-  // if (!isSignedIn) {
-  //   return <Navigate replace to="/isignin" />
-  // }
 
   return (
     <main className={styles.main}>
@@ -40,9 +37,9 @@ function Profile() {
         </div>
       </nav>
       <section className={styles.section}>
-        <div className="w-1/6 h-1/6 flex flex-col items-center justify-evenly">
-          <ProfileIcon size={50} className="h-12 w-12" />
-          <p className={styles.p}>{userAtom?.email ?? 'signin to view'}</p>
+        <div className={styles.info}>
+          <ProfileIcon size={50} className={styles.iconUser} />
+          <p className={styles.p}>{userAtom?.email ?? 'Sign in to view'}</p>
         </div>
       </section>
     </main>
