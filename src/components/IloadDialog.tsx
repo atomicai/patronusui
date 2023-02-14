2bda7e54113af1e025cefe1a6a4a4e7d1bcb1bfc
Dmitry Yurusov <dyurusov@gmail.com>
2023-02-14 19:35:57 +0300

    add combobox for column names

--- src/components/IloadDialog.tsx
+++ src/components/IloadDialog.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
-
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 import axios, { AxiosResponse } from 'axios'
 import { toast } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
+import { ColumnCandidates } from '../@types/view';
 
 type Response = {
   is_text_column_ok: boolean
@@ -18,15 +20,25 @@ interface Props {
   open: boolean
   setOpen: (value: boolean) => void
   handleViewing: any
+  columnCandidates: ColumnCandidates;
 }
 
-export default function IloadDialog({ open, setOpen, handleViewing }: Props) {
-  const [textValue, setTextValue] = useState('text')
-  const [dateValue, setDateValue] = useState('datetime')
+const labelStyle = { display: 'inline-block', width: '10em', marginRight: '0.5em' };
+const autocompleteStyle = { width: '15em' };
+const takeFirst = (values: string[] | undefined, defaultValue: string) => values?.length ? values[0] : defaultValue;
+
+export default function IloadDialog({ open, setOpen, handleViewing, columnCandidates }: Props) {
+  const [textValue, setTextValue] = useState(takeFirst(columnCandidates.text, 'text'));
+  const [dateValue, setDateValue] = useState(takeFirst(columnCandidates.datetime, 'datetime'));
   const [emailValue, setEmailValue] = useState<string>('')
   const [checkedResults, setCheckedResults] = useState(false)
   const [checkedSubscription, setCheckedSubscription] = useState(false)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    setTextValue(takeFirst(columnCandidates.text, 'text'));
+    setDateValue(takeFirst(columnCandidates.datetime, 'datetime'));
+  }, [columnCandidates]);
 
   const handleIloading = async () => {
     const response: AxiosResponse<Response> = await axios.post('/iloading', {
@@ -89,21 +101,28 @@ export default function IloadDialog({ open, setOpen, handleViewing }: Props) {
         </DialogTitle>
         <DialogContent sx={{ paddingBottom: 0 }}>
           <div className="flex flex-row items-center mb-1">
-            <span className="text-xl">Text column name: </span>{' '}
-            <input
+            <span className="text-xl" style={labelStyle}>Text column name: </span>
+            <Autocomplete
+              freeSolo
               value={textValue}
-              onChange={(e) => setTextValue(e.target.value)}
-              className="ml-3 text-base pl-2 text-field"
-              autoFocus={true}
+              onInputChange={(e, v) => setTextValue(v || '')}
+              options={columnCandidates.text || []}
+              renderInput={(params) => <TextField {...params} />}
+              size="small"
+              sx={autocompleteStyle}
             />
           </div>
 
           <div className="flex flex-row items-center">
-            <span className="text-xl">Date column name: </span>{' '}
-            <input
+            <span className="text-xl" style={labelStyle}>Date column name: </span>{' '}
+            <Autocomplete
+              freeSolo
               value={dateValue}
-              onChange={(e) => setDateValue(e.target.value)}
-              className="ml-3 text-base pl-2 text-field"
+              onInputChange={(e, v) => setDateValue(v || '')}
+              options={columnCandidates.datetime || []}
+              renderInput={(params) => <TextField {...params} />}
+              size="small"
+              sx={autocompleteStyle}
             />
           </div>
           <div className="mt-6 text-sm">
