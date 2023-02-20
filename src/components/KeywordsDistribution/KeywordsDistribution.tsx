3dcad1bcaf7a06feae96541d84121db86db43af2
Dmitry Yurusov <dyurusov@gmail.com>
2023-02-20 18:11:49 +0300

    refactor

--- src/components/KeywordsDistribution/KeywordsDistribution.tsx
+++ src/components/KeywordsDistribution/KeywordsDistribution.tsx
@@ -2,7 +2,6 @@ import { FC, SyntheticEvent, useEffect, useMemo, useState } from 'react';
 import { KeywordDistributionData, TimestampedValue } from '../../@types/search';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChart, Bar } from 'recharts';
 import { AxisDomain } from 'recharts/types/util/types';
-import styles from './KeywordsDistribution.module.css';
 
 interface KeywordsDistributionProps {
   data: KeywordDistributionData[];
@@ -13,12 +12,27 @@ enum KeywordsDistributionType {
   bar = 'bar',
 }
 
+const diagramColors = [
+  'cyan',
+  'orange',
+  'yellow',
+  'white',
+  'green',
+  'red',
+  'magenta',
+  'gray',
+  'brown',
+  'blue',
+];
+const diagramColorsLength = diagramColors.length;
+
 export const KeywordsDistribution: FC<KeywordsDistributionProps> = ({ data }) => {
   const [hidden, setHidden] = useState<number[]>([]);
   const [type, setType] = useState<KeywordsDistributionType>(KeywordsDistributionType.linear);
+  const [linearData, linearDomainX, linearDomainY] = useMemo(() => prepareData(data, KeywordsDistributionType.linear), [data]);
+  const [barData, barDomainX, barDomainY] = useMemo(() => prepareData(data, KeywordsDistributionType.bar), [data]);
+
   useEffect(() => setHidden([]), [data]);
-  const [linearData, linearDomainX, linearDomainY] = useMemo(() => prepareLinearData(data), [data]);
-  const [barData, barDomainX, barDomainY] = useMemo(() => prepareBarData(data), [data]);
 
   const toggleHidden =(e: SyntheticEvent, idx: number) => {
     if (!(e.target as HTMLInputElement).checked) {
@@ -44,10 +58,10 @@ export const KeywordsDistribution: FC<KeywordsDistributionProps> = ({ data }) =>
                 <Line
                   key={s.word}
                   dataKey="value"
-                  data={s.data} name={s.word}
+                  data={s.data} name={s.word as string}
                   hide={hidden.indexOf(idx) > -1}
                   strokeWidth={3}
-                  className={styles[`stroke${idx}`]}
+                  stroke={diagramColors[idx % diagramColorsLength]}
                 />
               ))}
             </LineChart>
@@ -61,7 +75,7 @@ export const KeywordsDistribution: FC<KeywordsDistributionProps> = ({ data }) =>
               <YAxis domain={barDomainY} />
               <Tooltip />
               {data.map((s, idx) => (
-                <Bar key={s.word} dataKey={s.word} hide={hidden.indexOf(idx) > -1} className={styles[`fill${idx}`]} />
+                <Bar key={s.word} dataKey={s.word} hide={hidden.indexOf(idx) > -1} fill={diagramColors[idx % diagramColorsLength]} />
               ))}
             </BarChart>
           </ResponsiveContainer>
@@ -71,7 +85,7 @@ export const KeywordsDistribution: FC<KeywordsDistributionProps> = ({ data }) =>
         <div>
           {data.map((item, idx) => (
             <div key={item.word} className="min-w-max">
-              <label className={`cursor-pointer ${styles[`label${idx}`]}`}>
+              <label className="cursor-pointer" style={{ color: diagramColors[idx % diagramColorsLength] }}>
                 <input type="checkbox" checked={hidden.indexOf(idx) === -1} onChange={(e) => toggleHidden(e, idx)}/>
                 <span className="ml-2">{item.word}</span>
               </label>
@@ -98,39 +112,13 @@ const yDelta = 1;
 
 const tickFormatter = (unixtime: number) => (new Date(unixtime)).toLocaleDateString('ru-RU');
 
-const prepareLinearData = (data: KeywordDistributionData[]) => {
-  const prepared = data.map(({ word, data }) => ({
-    word,
-    data: translateTimestamps(data || []).sort((a, b) => a.unixtime - b.unixtime),
-  }));
-  const minX: number[] = [];
-  const maxX: number[] = [];
-  const minY: number[] = [];
-  const maxY: number[] = [];
-  prepared.forEach(item => {
-    if (item.data.length) {
-      const unixtimes = item.data.map(({ unixtime }) => unixtime);
-      const values = item.data.map(({ value }) => value);
-      minX.push(Math.min(...unixtimes));
-      maxX.push(Math.max(...unixtimes));
-      minY.push(Math.min(...values));
-      maxY.push(Math.max(...values));
-    }
-  });
-  return [
-    prepared,
-    [Math.min(...minX) - xDelta, Math.max(...maxX) + xDelta] as AxisDomain,
-    [Math.min(...minY) - yDelta, Math.max(...maxY) + yDelta] as AxisDomain,
-  ] as const;
-}
-
 const translateTimestamps = (data: TimestampedValue[]) => data.map(({ value, timestamp }) => ({
   value,
   unixtime: (new Date(timestamp)).valueOf(),
 }));
 
-const prepareBarData = (data: KeywordDistributionData[]) => {
-  const prepared = data.map(({ word, data }) => ({
+const prepareData = (data: KeywordDistributionData[], type: KeywordsDistributionType) => {
+  const transformed = data.map(({ word, data }) => ({
     word,
     data: translateTimestamps(data || []).sort((a, b) => a.unixtime - b.unixtime),
   }));
@@ -139,7 +127,7 @@ const prepareBarData = (data: KeywordDistributionData[]) => {
   const minY: number[] = [];
   const maxY: number[] = [];
   const barData: Record<number, Record<string, number>> = {};
-  prepared.forEach(item => {
+  transformed.forEach(item => {
     if (item.data.length) {
       const unixtimes = item.data.map(({ unixtime }) => unixtime);
       const values = item.data.map(({ value }) => value);
@@ -147,15 +135,19 @@ const prepareBarData = (data: KeywordDistributionData[]) => {
       maxX.push(Math.max(...unixtimes));
       minY.push(Math.min(...values));
       maxY.push(Math.max(...values));
-      item.data.forEach(s => {
-        const unixtime = s.unixtime;
-        barData[unixtime] = { ...barData[unixtime], unixtime, [item.word]: s.value };
-      });
+      if (type === KeywordsDistributionType.bar) {
+        item.data.forEach(s => {
+          const unixtime = s.unixtime;
+          barData[unixtime] = { ...barData[unixtime], unixtime, [item.word]: s.value };
+        });
+      }
     }
   });
   return [
-    Object.values(barData).sort((a, b) => a.unixtime - b.unixtime),
+    (type === KeywordsDistributionType.bar)
+      ? Object.values(barData).sort((a, b) => a.unixtime - b.unixtime)
+      : transformed,
     [Math.min(...minX) - xDelta, Math.max(...maxX) + xDelta] as AxisDomain,
     [Math.min(...minY) - yDelta, Math.max(...maxY) + yDelta] as AxisDomain,
   ] as const;
-}
+};
