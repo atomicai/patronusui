b8a3b613e14ab277aa16cbc962ae5199cef15389
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-03-05 20:58:24 +0300

    Add simplified history and settings tabs to `Profile` page

--- src/pages/user/Profile.tsx
+++ src/pages/user/Profile.tsx
@@ -1,19 +1,53 @@
 import {
   ArrowRightOnRectangleIcon,
+  BookOpenIcon,
+  Cog6ToothIcon,
   HomeIcon,
   MagnifyingGlassIcon,
   PresentationChartBarIcon
 } from '@heroicons/react/24/outline'
 import { signOut } from 'firebase/auth'
 import { useAtom } from 'jotai'
+import { nanoid } from 'nanoid'
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import ProfileIcon from '../../components/ProfileIcon'
 import { currentUser } from '../../contexts/AuthContext'
 import { auth } from '../../firebase'
+
+import { Tab } from '../../@types/user'
+import TabElement from './components/TabElement/TabElement'
+import TabPanel from './components/TabPanel/TabPanel'
+
 import styles from './components/styles/Profile.module.css'
+
+const tabs: Tab[] = [
+  { idx: 0, header: 'History', icon: <BookOpenIcon /> },
+  { idx: 0, header: 'Settings', icon: <Cog6ToothIcon /> }
+]
 
 function Profile() {
   const [userAtom] = useAtom(currentUser)
+  const [idxActive, setIdxActive] = useState<number>(0)
+
+  const searchQueries = userAtom?.searchHistory || []
+
+  const searchQueryElems = searchQueries.map(
+    (searchQuery: string): JSX.Element => (
+      <div className={styles.searchQuery} key={nanoid()}>
+        {searchQuery}
+      </div>
+    )
+  )
+
+  const tabElements = [
+    <TabElement description="Search History" children={searchQueryElems} />,
+    <TabElement description="User Settings" />
+  ]
+
+  const pickTab = (e: React.MouseEvent<HTMLElement>, idx: number) => {
+    setIdxActive(idx)
+  }
 
   return (
     <main className={styles.main}>
@@ -36,11 +70,13 @@ function Profile() {
           </button>
         </div>
       </nav>
+      <TabPanel tabs={tabs} idxAtive={idxActive} handleClick={pickTab} />
       <section className={styles.section}>
-        <div className={styles.info}>
-          <ProfileIcon size={50} className={styles.iconUser} />
+        <div className={styles.sectionUser}>
+          <ProfileIcon size={192} className={styles.iconUser} />
           <p className={styles.p}>{userAtom?.email ?? 'Sign in to view'}</p>
         </div>
+        {tabElements[idxActive]}
       </section>
     </main>
   )
